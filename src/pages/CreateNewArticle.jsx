331931210e21f9b32923cb437859a4ffcd763cac
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-11-29 12:20:44 +0100

    Notification box

--- src/pages/CreateNewArticle.jsx
+++ src/pages/CreateNewArticle.jsx
@@ -1,9 +1,12 @@
 import {useState, useRef, useEffect} from "react";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import axios  from 'axios'
 
 const CreateNewArticle = () => {
+  const [photos, setPhotos] = useState([])
   const [uploadImg, setUploadImg] = useState(false)
+  const [uploadImgFrom, setUploadImgFrom] = useState('local')
   const inputUpload = useRef()
   const [selectedImg, setSelectedImg] = useState(undefined)
   const [imgPreview, setImgPreview] = useState(undefined)
@@ -13,47 +16,102 @@ const CreateNewArticle = () => {
       return
   }
     setSelectedImg(e.target.files[0])
-    console.log(selectedImg)
+    //console.log(selectedImg)
   }
   useEffect(()=>{
     if (!selectedImg) {
       setImgPreview(undefined)
       return
-  }
-  setUploadImg(false)
+    }
+    setUploadImg(false)
     const objectUrl = URL.createObjectURL(selectedImg)
     setImgPreview(objectUrl)
     // free memory when ever this component is unmounted
     return () => URL.revokeObjectURL(objectUrl)
   },[selectedImg])
   
+  const fetchPhotos = ()=>{
+    axios({
+      method: 'get',
+      url: 'https://api.unsplash.com/photos/',
+      headers: { Authorization: "Client-ID 1yZA8BKiDDm37swWqYwX-O6pswpYU16pgxHtDu3A3i8", },
+    }).then((response)=>{
+      console.log(response.data[0].user['first_name'] + ' ' + response.data[0].user['last_name'])
+      setPhotos(response.data) 
+    }).catch((error)=>{
+      console.log(error)
+    })
+  }
+
   return (
     <div className="bg-white h-full mt-[-20px]">
-    <div className="lg:w-[80vw] w-[98vw] sm:w-[88vw] md:w-[78vw] xl:w-[65vw]  mx-auto  mt-12 ">
+    <div className="lg:w-[80vw] w-[98vw] sm:w-[88vw] md:w-[78vw] xl:w-[65vw] mx-auto mt-12 ">
         <div className="flex flex-col gap-8">
           <div className="text-[#334155] flex gap-4 relative">
             {/*Add Cover Box */}
-            <div className={`${uploadImg ? 'flex' : 'hidden'} w-[80%] flex-col border absolute top-[100%] left-0 z-50 p-4 max-w-[450px] min-w-[320px] bg-white rounded-lg shadow-lg shadow-slate-200`}>
+            <div className={`${uploadImg ? 'flex' : 'hidden'} w-[80%] flex-col border absolute top-[100%] left-0 z-50 p-4 max-w-[950px] min-w-[320px] bg-white rounded-lg shadow-lg shadow-slate-200`}>
               <button onClick={()=>setUploadImg(false)} type="button" variant="transparent" aria-label="Hide cover image modal" class="opacity-75 hover:bg-[#e2e8f0] absolute right-0 top-0 py-1 px-3 leading-relaxed font-medium rounded-full align-middle flex justify-center items-center m-2 outline-transparent outline outline-2 outline-offset-2">
                 <svg class=" w-5 h-5" viewBox="0 0 320 512"><path d="M193.94 256L296.5 153.44l21.15-21.15c3.12-3.12 3.12-8.19 0-11.31l-22.63-22.63c-3.12-3.12-8.19-3.12-11.31 0L160 222.06 36.29 98.34c-3.12-3.12-8.19-3.12-11.31 0L2.34 120.97c-3.12 3.12-3.12 8.19 0 11.31L126.06 256 2.34 379.71c-3.12 3.12-3.12 8.19 0 11.31l22.63 22.63c3.12 3.12 8.19 3.12 11.31 0L160 289.94 262.56 392.5l21.15 21.15c3.12 3.12 8.19 3.12 11.31 0l22.63-22.63c3.12-3.12 3.12-8.19 0-11.31L193.94 256z"></path></svg>
               </button>
               <div class="flex gap-4 w-full border-b">
-                <button type="button" variant="no-style" aria-label="Upload an image for the cover" class="flex  gap-2 flex-nowrap items-center justify-center outline-transparent outline-2 outline-offset-2 py-3 px-4 font-semibold border-b-2 rounded-t-md text-[#3466f6] border-[#3466f6] hover:bg-[#e2e8f0]">
-                  <svg fill="#3466f6" class="w-5 h-5" viewBox="0 0 512 512"><path d="M122.6 155.1 240 51.63V368c0 8.844 7.156 16 16 16s16-7.156 16-16V51.63l117.4 104.3c3 2.77 6.8 4.07 10.6 4.07 4.406 0 8.812-1.812 11.97-5.375 5.875-6.594 5.25-16.72-1.344-22.58l-144-128a15.949 15.949 0 0 0-21.25 0l-144 128C94.78 137.9 94.16 148 100 154.6s16.1 7.2 22.6.5zM448 320H336c-8.836 0-16 7.162-16 16 0 8.836 7.164 16 16 16h112c17.67 0 32 14.33 32 32v64c0 17.67-14.33 32-32 32H64c-17.67 0-32-14.33-32-32v-64c0-17.67 14.33-32 32-32h112c8.8 0 16-7.2 16-16s-7.2-16-16-16H64c-35.35 0-64 28.65-64 64v64c0 35.35 28.65 64 64 64h384c35.35 0 64-28.65 64-64v-64c0-35.3-28.7-64-64-64zm-8 96c0-13.25-10.75-24-24-24s-24 10.75-24 24 10.75 24 24 24 24-10.7 24-24z"></path></svg>
+                <button onClick={()=>setUploadImgFrom('local')} type="button" variant="no-style" aria-label="Upload an image for the cover" class={` ${uploadImgFrom === 'local' ? 'text-[#3466f6] border-[#3466f6] border-b-2' : ''} flex  gap-2 flex-nowrap items-center justify-center outline-transparent outline-2 outline-offset-2 py-3 px-4 font-semibold rounded-t-md hover:bg-[#e2e8f0]`}>
+                  <svg fill={` ${uploadImgFrom === 'local' ? '#3466f6' : ''} `} class={`w-5 h-5 ${uploadImgFrom === 'local' ? '' : 'opacity-75'} `} viewBox="0 0 512 512"><path d="M122.6 155.1 240 51.63V368c0 8.844 7.156 16 16 16s16-7.156 16-16V51.63l117.4 104.3c3 2.77 6.8 4.07 10.6 4.07 4.406 0 8.812-1.812 11.97-5.375 5.875-6.594 5.25-16.72-1.344-22.58l-144-128a15.949 15.949 0 0 0-21.25 0l-144 128C94.78 137.9 94.16 148 100 154.6s16.1 7.2 22.6.5zM448 320H336c-8.836 0-16 7.162-16 16 0 8.836 7.164 16 16 16h112c17.67 0 32 14.33 32 32v64c0 17.67-14.33 32-32 32H64c-17.67 0-32-14.33-32-32v-64c0-17.67 14.33-32 32-32h112c8.8 0 16-7.2 16-16s-7.2-16-16-16H64c-35.35 0-64 28.65-64 64v64c0 35.35 28.65 64 64 64h384c35.35 0 64-28.65 64-64v-64c0-35.3-28.7-64-64-64zm-8 96c0-13.25-10.75-24-24-24s-24 10.75-24 24 10.75 24 24 24 24-10.7 24-24z"></path></svg>
                   <span>Upload</span>
                 </button>
-                <button  type="button" variant="no-style" aria-label="Choose an Unsplash image for the cover" class="flex opacity-75 gap-2 flex-nowrap items-center justify-center outline-transparent outline-2 outline-offset-2 py-3 px-4 font-semibold rounded-t-md  hover:bg-[#e2e8f0]">
-                  <svg class="w-5 h-5 opacity-75" viewBox="20 21 96 96"><g fill-rule="evenodd"><path d="M50 21h36v27H50zM20 63h30.087v27.24h36.079V63H116v54H20z"></path></g></svg>
+                <button onClick={()=>{setUploadImgFrom('unsplash'); fetchPhotos()}} type="button" variant="no-style" aria-label="Choose an Unsplash image for the cover" class={` ${uploadImgFrom === 'unsplash' ? 'text-[#3466f6] border-[#3466f6] border-b-2' : ''} flex gap-2 flex-nowrap items-center justify-center outline-transparent outline-2 outline-offset-2 py-3 px-4 font-semibold rounded-t-md  hover:bg-[#e2e8f0]` }>
+                  <svg fill={` ${uploadImgFrom === 'unsplash' ? '#3466f6' : ''} `} class={`w-5 h-5 ${uploadImgFrom === 'unsplash' ? '' : 'opacity-75'} `} viewBox="20 21 96 96"><g fill-rule="evenodd"><path d="M50 21h36v27H50zM20 63h30.087v27.24h36.079V63H116v54H20z"></path></g></svg>
                   <span>Unsplash</span>
                 </button>
               </div>
-              <div class="flex justify-center  items-center align-middle mt-10 mb-6">
-                <label class="py-3 px-12 text-sm font-bold rounded-lg text-white bg-blue-600 cursor-pointer hover:bg-opacity-75">
-                  <span class="">Choose an image</span>
-                  <input onChange={onSelectFile} className="hidden" type="file" ref={inputUpload} data-id="upload-cover" accept="image/apng, image/avif, image/gif, image/jpeg, image/png, image/svg, image/webp, image/bmp, image/x, image/tiff, image/vnd, image/xbm" />
-                </label>
+              {uploadImgFrom === 'local' ?
+              <>
+                <div class="flex justify-center  items-center align-middle mt-10 mb-6">
+                  <label class="py-3 px-12 text-sm font-bold rounded-lg text-white bg-blue-600 cursor-pointer hover:bg-opacity-75">
+                    <span class="">Choose an image</span>
+                    <input onChange={onSelectFile} className="hidden" type="file" ref={inputUpload} data-id="upload-cover" accept="image/apng, image/avif, image/gif, image/jpeg, image/png, image/svg, image/webp, image/bmp, image/x, image/tiff, image/vnd, image/xbm" />
+                  </label>
+                </div>
+                  <p class="text-sm w-full text-center text-[#475569] leading-tight">Recommended dimension is 1600 x 840</p>
+              </>
+              :
+              <div className="flex flex-col ">
+                <div className="mt-6">
+                  <label for="unsplash-search" class="flex gap-3 w-full mb-5">
+                    <div class="w-11/12">
+                      <input className="rounded-md outline-transparent outline outline-2 outline-offset-2 border py-2 px-4 text-sm w-full bg-[#f8fafc] focus:bg-transparent focus:border-[#3466f6]" id="unsplash-search" type="text" autocomplete="off" placeholder="Type something and press enter"  value="" />
+                      <button className="" disabled="" type="button" variant="no-style" aria-label="Clear image search">
+                        <svg class="" viewBox="0 0 320 512"><path d="M193.94 256L296.5 153.44l21.15-21.15c3.12-3.12 3.12-8.19 0-11.31l-22.63-22.63c-3.12-3.12-8.19-3.12-11.31 0L160 222.06 36.29 98.34c-3.12-3.12-8.19-3.12-11.31 0L2.34 120.97c-3.12 3.12-3.12 8.19 0 11.31L126.06 256 2.34 379.71c-3.12 3.12-3.12 8.19 0 11.31l22.63 22.63c3.12 3.12 8.19 3.12 11.31 0L160 289.94 262.56 392.5l21.15 21.15c3.12 3.12 8.19 3.12 11.31 0l22.63-22.63c3.12-3.12 3.12-8.19 0-11.31L193.94 256z"></path></svg>
+                      </button>
+                    </div>
+                    <button className="py-1 hover:bg-[#e2e8f0] px-3 font-medium text-[#334155] border-transparent border outline-transparent outline-2 outline-offset-2 rounded-md" type="button" variant="transparent" >
+                      <svg fill="" class="w-4 h-4" viewBox="0 0 512 512"><path fill="currentColor" d="M507.3 484.7 365.8 343.2c31.2-36.4 49.3-83.5 49.3-135.2 0-114.9-93.13-208-208-208S0 93.13 0 208s93.12 208 207.1 208c51.68 0 98.85-18.96 135.2-50.15l141.5 141.5c4 3.05 8.1 4.65 12.2 4.65s8.188-1.562 11.31-4.688c6.29-6.212 6.29-16.412-.01-22.612zM208 384c-97.9 0-176-79-176-176S110.1 32 208 32s176 78.1 176 176-79 176-176 176z"></path></svg>
+                    </button>
+                  </label>
+                </div>
+                
+                <div className="w-full pr-4 grid grid-cols-8 gap-4 whitespace-wrap overflow-y-auto h-[18rem]">
+                {
+            photos.map((photo)=>{
+              //console.log(photo.user['first_name'])
+              return (
+                    <div className="col-span-4 rounded-md cursor-pointer whitespace-normal">
+                      <button className="w-full hover:border-[#3466f6] overflow-hidden border-2 rounded-md outline-transparent outline-2 outline-offset-2 bg-transparent">
+                        <div className="relative pb-[56.25%] w-full">
+                          <div className="absolute inset-0 ">
+                              <img src={photo.urls['small']} key={photo['id']} alt="z"  srcset="" className="w-full h-full" />
+                          </div>
+                        </div>
+                      </button>
+                      <p className="">
+                        <a href="g">by <span className="font-bold">{photo.user['first_name'] + ' ' + photo.user['last_name']}</span></a>
+                      </p>
+                    </div>
+              )
+            })
+          }
+          </div>
               </div>
-               <p class="text-sm w-full text-center text-[#475569] leading-tight">Recommended dimension is 1600 x 840</p>
+            }
             </div>
 
 
@@ -88,8 +146,9 @@ const CreateNewArticle = () => {
         </div>
 
         <SimpleMDE />
-
-        <button className="bg-blue-600 mb-8 rounded-full py-2 px-5 text-white font-medium text-lg">Publish</button>
+          
+        <button className="bg-blue-600 mb-8 rounded-full py-2 px-5 text-white font-medium text-lg mr-4">Publish</button>
+        <button className="bg-gray-800 mb-8 rounded-full py-2 px-5 text-white font-medium text-lg">Save as Draft</button>
         
     </div>
     </div>
